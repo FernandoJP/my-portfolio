a306a22b7f49ab18de3c9a567dce6e56a10b6d91
Fernando de Paula <fernando.opus@gmail.com>
2021-02-07 10:55:37 -0300

    Animated stars added

--- src/assets/js/skills.js
+++ src/assets/js/skills.js
@@ -1,8 +1,19 @@
 window.addEventListener('scroll', () => {
-    const value = window.scrollY;
-    const skillsHtml = document.querySelector('.skills__bar-html');
-    console.log(window.scrollY);
-    const barWidth = (value-200) / 6;
-    const updatedBarWidth = barWidth > 100 ? 100 : barWidth;
-    skillsHtml.style.width = `calc(${updatedBarWidth}% - 9.0rem)`;
+    const skillsEl = document.querySelectorAll('.skills__html');
+    const scrollY = window.scrollY / 100 - 3;
+    console.log(window.scrollY, scrollY);
+    //skillsEl.style.width = `calc(${updatedBarWidth}% - 9.0rem)`;
+
+    skillsEl.forEach((element, i) => {
+        console.log(element, i);
+        element.querySelectorAll('.stars__star').forEach((star, j) => {
+            if(scrollY-i > j) {
+                star.classList.remove('stars__star--hide')
+                !star.classList.contains('stars__star--visible') && star.classList.add('stars__star--visible');
+            } else {
+                star.classList.remove('stars__star--visible')
+                !star.classList.contains('stars__star--hide') && star.classList.add('stars__star--hide');
+            }
+        });
+    });
 });
